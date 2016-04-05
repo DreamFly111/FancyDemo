32736e2ffcae414166cfcfaf7fad89d52865f033
wyx111 <2538066216@qq.com>
2016-04-05 17:08:44 +0800

    butterknife

--- app/src/main/java/duoy/cn/fancydemo/MainActivity.java
+++ app/src/main/java/duoy/cn/fancydemo/MainActivity.java
@@ -1,42 +1,51 @@
 package duoy.cn.fancydemo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import duoy.cn.fancydemo.activity.BorrowActivity;
 import duoy.cn.fancydemo.activity.LendActivity;
 import duoy.cn.fancydemo.activity.QueryActivity;
+import duoy.cn.fancydemo.utils.IntentUtils;
 
 public class MainActivity extends AppCompatActivity{
 
-    private Button borrow;
-    private Button lend;
-    private Button query;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.borrow)
+    TextView borrow;
+    @Bind(R.id.lend)
+    TextView lend;
+    @Bind(R.id.query)
+    TextView query;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        borrow = (Button) findViewById(R.id.borrow);
-        lend = (Button) findViewById(R.id.lend);
-        query = (Button) findViewById(R.id.query);
+        ButterKnife.bind(this);
 
     }
 
-    public void borrow(View view){
-        Intent intent = new Intent(this,BorrowActivity.class);
-        startActivity(intent);
-    }
-
-    public void lend(View view){
-        Intent intent = new Intent(this,LendActivity.class);
-        startActivity(intent);
-    }
-
-    public void query(View view){
-        Intent intent = new Intent(this,QueryActivity.class);
-        startActivity(intent);
+    @OnClick({R.id.borrow,R.id.lend,R.id.query})
+    public void onClicd(View view){
+        switch (view.getId()){
+            case R.id.borrow:
+                startActivity(IntentUtils.generateIntent(this, BorrowActivity.class));
+                break;
+            case R.id.lend:
+                startActivity(IntentUtils.generateIntent(this,LendActivity.class));
+                break;
+            case R.id.query:
+                startActivity(IntentUtils.generateIntent(this,QueryActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
